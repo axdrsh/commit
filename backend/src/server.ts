7965a1764329f4290320b5d58e2522f509c12311
adarsh <axdrsh@gmail.com>
2025-08-29 20:53:04 +0000

    setup backend and postgers with prisma

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.listen(4000, () => console.log("server running on 4000"));
